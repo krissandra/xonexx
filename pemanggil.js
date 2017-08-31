dc3052b1ba2e81f42c6ef5edd9907fbd271f64fc
krissandra <31430349+krissandra@users.noreply.github.com>
2017-08-31 15:46:49 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -26,7 +26,7 @@ if(x=="1"){xxx="/2017/08/bahaya-memakai-headset-bagi-kesehatan.html"}
 if(x=="2"){xxx="/2017/08/bagian-tubuh-yang-seringkali-dilupakan.html"}
 if(x=="3"){xxx="/2017/08/tips-mengatasi-mata-sembab-di-pagi-hari.html"}
 if(x=="4"){xxx="/2017/08/6-cara-terbaik-mengkontrol-berat-badan.html"}
-if(x=="5"){xxx="/2017/08/cara-mengatasi-perut-mual-secara-alami.html"}a_to_ve[i].href="https://cariinformasiuntuk.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/08/cara-mengatasi-perut-mual-secara-alami.html"}a_to_ve[i].href="http://www.cariinformasiuntuk.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
